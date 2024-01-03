9974bbc87d777764929ab9ab2238bca8daf4a07d
Jayden Pahukula <72293021+JaydenPahukula@users.noreply.github.com>
2024-01-02 23:32:49 -1000

    barebones

--- src/App.svelte
+++ src/App.svelte
@@ -1,47 +1,41 @@
 <script>
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+  import './app.css'
+  // import Counter from './lib/Counter.svelte'
 </script>
 
+
 <main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
+  <div class="headerdiv">
+    <div>
+      <p class="titleline1">Hello, my name is</p>
+      <h1 class="titleline2">Jayden Pahukula</h1>
+    </div>
   </div>
 
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
+  <div style="height:10000px" />
 
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
 </main>
 
+
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
+  main {
+    background-color: rgb(1, 65, 95);
   }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
+  .headerdiv {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    height: 100vh;
+    text-align: center;
   }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
+  .titleline1 {
+    font-size: 3rem;
+    margin: 0;
   }
-  .read-the-docs {
-    color: #888;
+  .titleline2 {
+    font-size: 5rem;
+    margin: 0;
+    margin-top: 1rem;
+    
   }
 </style>
