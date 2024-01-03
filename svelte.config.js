6dc57de1a5803496afb54baffe595d5ba0663abb
Jayden Pahukula <72293021+JaydenPahukula@users.noreply.github.com>
2024-01-02 18:55:37 -1000

    integrate with gh pages

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,12 @@
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
 
-export default {
-  // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
-  // for more information about preprocessors
-  preprocess: vitePreprocess(),
-}
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+  kit: {
+      appDir: 'app', // Required as the default is _app
+      adapter: adapter()
+  },
+  preprocess: vitePreprocess()
+};
+export default config;
